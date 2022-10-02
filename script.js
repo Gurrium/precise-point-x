ab5b4369b8846a49b303d9f791c5ed6b8a14bae3
gurrium <gurrium@hatena.ne.jp>
2022-10-02 20:38:41 +0900

    refine

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-queryObjects(SampleGraph)
 var orig = SampleGraph.prototype.repositionPointHighlight
-SampleGraph.prototype.repositionPointHighlight = () => { orig.call(temp1); var s = temp1.xAxisType(); temp1.$hoverXLabel.html(temp1.metrics(s).convert(temp1._pointHighlighted.point[s])) }
+SampleGraph.prototype.repositionPointHighlight = function() {
+    orig.bind(this)()
+
+    var s = this.xAxisType()
+    var n = this.metrics(s).convert(this._pointHighlighted.point[s])
+    this.$hoverXLabel.html(n)
+}
