2e294f468adce47e572530218e4ef378cf0c78a3
gurrium <gurrium@hatena.ne.jp>
2023-04-25 22:32:42 +0900

    データは素朴に取れたが微妙…

--- content.js
+++ content.js
@@ -1,7 +1,13 @@
+function getRoute() {
+  return new Promise(resolve => {
+    $.getJSON(`${window.location.href}.json`, data => resolve(data))
+  })
+}
+
+let route
+getRoute().then(r => { console.log(r); route = r })
+
 rwgps.Events.bind("sg:highlightPoint", function(e) {
-  // TODO: data[e.x]で欲しい物が取れそう。
-  // あとはdataをどうにかして取ってくる。
-  // JSONを取ってきて地図上に表示していた記憶があるのでそこを除くか、何かしらのイベントにbindするか
-  console.log(this, e)
+  console.log(route.track_points[e.x])
 })
 
